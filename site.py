2899314a75f5c0539c22b9a7a780307baaf77926
viniiDev18 <viniciusamaral016@gmail.com>
2024-09-06 13:18:45 -0300

    Add files via upload

---
+++ site.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# route => domínio do seu site, ex: /contatos, /homepage.
+# função => o que você quer exibir naquela página.
+
+# colocar o site no ar
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+
+@app.route("/usuarios/<nome_usuario>") # <> => flask ler o nome de usuário
+def usuarios(nome_usuario): # define a variável e joga para o html
+    return render_template("usuarios.html", nome_usuario=nome_usuario) # retorna a variável descrita em html
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# colocar nosso site para pessoas acessarem
